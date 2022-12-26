b60bb2c76233e2ec0a0990b8ed19a2bd4ae8a21a
metagigachad <288993@gmail.com>
2022-12-26 11:16:21 +0300

    Added signal activation

--- blocks.def.h
+++ blocks.def.h
@@ -2,11 +2,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	//{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-  {"", "cat ~/.cache/status/layout.txt",           1,    0},
-  {"", "~/.local/bin/statusbar/network-status.sh", 1,    0},
-  {"", "~/.local/bin/statusbar/audio-status.sh",   1,    0},
-  {"", "~/.local/bin/statusbar/battery-status.sh", 1,    0},
-	{"", "date '+%b %d (%a) %I:%M%p'",					     1,		 0},
+  {"", "~/.local/bin/statusbar/network-status.sh", 1,     0},
+  {"", "cat ~/.cache/status/layout.txt",           60,    11},
+  {"", "~/.local/bin/statusbar/audio-status.sh",   60,    10},
+  {"", "~/.local/bin/statusbar/battery-status.sh", 1,     0},
+	{"", "date '+%b %d (%a) %H:%M'",					       5,		  0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
